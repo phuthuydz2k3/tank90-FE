2d0ddb5d5137112eb8d8fef84c9fd7add25cb128
Duy Nguyễn <duykhanhchi1993@gmail.com>
2024-12-25 20:36:53 +0700

    fix: delay tank by deleting while loop

--- Source/Game/Systems/NetworkReceiverSystem.cpp
+++ Source/Game/Systems/NetworkReceiverSystem.cpp
@@ -21,61 +21,63 @@ boost::asio::io_context io_context3;
 void NetworkReceiverSystem::update() {
     System::update();
     NetworkReceiver::clientSocket.non_blocking(true);
-    boost::system::error_code error;
-    size_t len = NetworkReceiver::clientSocket.receive(boost::asio::buffer(NetworkReceiver::recvBuffer), 0, error);
+    while (true) {
+        boost::system::error_code error;
+        size_t len = NetworkReceiver::clientSocket.receive(boost::asio::buffer(NetworkReceiver::recvBuffer), 0, error);
 
-    if (error && error != boost::asio::error::message_size) {
-        if (error == boost::asio::error::would_block || error == boost::asio::error::try_again) {
-            // No data received, break the loop
-            return;
-        } else {
-            std::cerr << "Receive failed: " << error.message() << std::endl;
-            return;
-        }
-    }
-
-    if (len % sizeof(TankStatePacket) != 0) {
-        std::cerr << "Received packet size mismatch" << std::endl;
-        return;
-    }
-
-    std::vector<TankStatePacket> receivedTankStates;
-    int numPackets = len / sizeof(TankStatePacket);
-    for (int i = 0; i < numPackets; ++i) {
-        TankStatePacket receivedPacket;
-        std::memcpy(&receivedPacket, NetworkReceiver::recvBuffer.data() + i * sizeof(TankStatePacket),
-                    sizeof(TankStatePacket));
-        receivedTankStates.push_back(receivedPacket);
-    }
-
-    // Print received tank states
-    auto entities = EntityManager::getInstance()->getEntitiesWithComponent<NetworkReceiver>();
-    for (const auto &tankState: receivedTankStates) {
-        bool haveTank = false;
-        if (tankState.id == NetworkTracking::id) continue;
-        for (const auto &entity: entities) {
-            NetworkReceiver *networkReceiver = entity->getComponent<NetworkReceiver>();
-            std:: cout << networkReceiver->id << " "<< tankState.id << std::endl;
-            if (networkReceiver->id == tankState.id) {
-                haveTank = true;
-                std::cout << "Found tank with id: " << tankState.id << std::endl;
-                entity->getComponent<Transform>()->position = VECTOR2(tankState.positionX, tankState.positionY);
-                entity->getComponent<Transform>()->angle = tankState.angle;
+        if (error && error != boost::asio::error::message_size) {
+            if (error == boost::asio::error::would_block || error == boost::asio::error::try_again) {
+                // No data received, break the loop
+                break;
+            } else {
+                std::cerr << "Receive failed: " << error.message() << std::endl;
                 break;
             }
         }
-        std:: cout << entities.size() << std::endl;
-        if (!haveTank) {
-            Tank *tank = EntityManager::getInstance()->createEntity<Tank>();
-            tank->removeComponent<ControlComponent>();
-            tank->removeComponent<NetworkTracking>();
-            tank->getComponent<Transform>()->position = VECTOR2(tankState.positionX, tankState.positionY);
-            tank->getComponent<Transform>()->angle = tankState.angle;
-            tank->addComponent<NetworkReceiver>();
-            tank->getComponent<NetworkReceiver>()->id = tankState.id;
+
+        if (len % sizeof(TankStatePacket) != 0) {
+            std::cerr << "Received packet size mismatch" << std::endl;
+            break;
         }
-        std::cout << "Received tank state: " << tankState.id << " " << tankState.positionX << " " << tankState.positionY
-                << " " << tankState.angle << std::endl;
+
+        std::vector<TankStatePacket> receivedTankStates;
+        int numPackets = len / sizeof(TankStatePacket);
+        for (int i = 0; i < numPackets; ++i) {
+            TankStatePacket receivedPacket;
+            std::memcpy(&receivedPacket, NetworkReceiver::recvBuffer.data() + i * sizeof(TankStatePacket),
+                        sizeof(TankStatePacket));
+            receivedTankStates.push_back(receivedPacket);
+        }
+
+        // Print received tank states
+        auto entities = EntityManager::getInstance()->getEntitiesWithComponent<NetworkReceiver>();
+        for (const auto &tankState: receivedTankStates) {
+            bool haveTank = false;
+            if (tankState.id == NetworkTracking::id) continue;
+            for (const auto &entity: entities) {
+                NetworkReceiver *networkReceiver = entity->getComponent<NetworkReceiver>();
+                std:: cout << networkReceiver->id << " "<< tankState.id << std::endl;
+                if (networkReceiver->id == tankState.id) {
+                    haveTank = true;
+                    std::cout << "Found tank with id: " << tankState.id << std::endl;
+                    entity->getComponent<Transform>()->position = VECTOR2(tankState.positionX, tankState.positionY);
+                    entity->getComponent<Transform>()->angle = tankState.angle;
+                    break;
+                }
+            }
+            std:: cout << entities.size() << std::endl;
+            if (!haveTank) {
+                Tank *tank = EntityManager::getInstance()->createEntity<Tank>();
+                tank->removeComponent<ControlComponent>();
+                tank->removeComponent<NetworkTracking>();
+                tank->getComponent<Transform>()->position = VECTOR2(tankState.positionX, tankState.positionY);
+                tank->getComponent<Transform>()->angle = tankState.angle;
+                tank->addComponent<NetworkReceiver>();
+                tank->getComponent<NetworkReceiver>()->id = tankState.id;
+            }
+            std::cout << "Received tank state: " << tankState.id << " " << tankState.positionX << " " << tankState.positionY
+                    << " " << tankState.angle << std::endl;
+        }
     }
 }
 
